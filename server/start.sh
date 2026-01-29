7c88975657beacd3aac9a700ff89b509aff89d03
Saidul Islam Shehab <saidulislamshehab@gmail.com>
2026-01-30 02:47:27 +0600

    .Disable route caching for debugging

--- server/start.sh
+++ server/start.sh
@@ -6,8 +6,8 @@ composer install --no-dev --working-dir=/var/www/html
 echo "Caching config..."
 php artisan config:cache
 
-echo "Caching routes..."
-php artisan route:cache
+# echo "Caching routes..."
+# php artisan route:cache
 
 echo "Running migrations..."
 php artisan migrate --force
