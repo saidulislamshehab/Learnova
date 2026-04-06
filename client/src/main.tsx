6abc7cdf8289005bcb622fce6259e4e3fc06d1b3
Saidul Islam Shehab <saidulislamshehab@gmail.com>
2026-04-07 00:24:00 +0600

    Add admin theme styles and integrate into main application

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -5,6 +5,7 @@ import App from "./App.tsx";
 import { Toaster } from "./components/ui/sonner";
 import "./index.css";
 import "./styles/components.css";
+import "./styles/admin-theme.css";
 
 createRoot(document.getElementById("root")!).render(
   <BrowserRouter>
